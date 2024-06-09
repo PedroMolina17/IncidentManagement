b3c1e2244b216ccc38533c54f40edf76bbde0f8d
pedromolina17 <pedro.molinanoa@gmail.com>
2024-06-08 22:59:41 -0500

    Prisma Module

--- src/incident-types/incident-types.module.ts
+++ src/incident-types/incident-types.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { IncidentTypesService } from './incident-types.service';
 import { IncidentTypesController } from './incident-types.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
   controllers: [IncidentTypesController],
   providers: [IncidentTypesService],
+  imports: [PrismaModule],
 })
 export class IncidentTypesModule {}
