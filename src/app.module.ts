9f4c47b4702bbecf9c73f8558156e557c41ea5a5
pedromolina17 <pedro.molinanoa@gmail.com>
2024-06-11 22:09:13 -0500

    Add User Api

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { IncidentTypesModule } from './incident-types/incident-types.module';
 import { TypeUsersModule } from './type-users/type-users.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { StatusModule } from './status/status.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [IncidentTypesModule, TypeUsersModule, PrismaModule, StatusModule],
+  imports: [IncidentTypesModule, TypeUsersModule, PrismaModule, StatusModule, UsersModule],
   controllers: [],
   providers: [],
 })
