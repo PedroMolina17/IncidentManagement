b124a22c8c6ce3f1a330cf9c57f10f7b61c9f3d2
pedromolina17 <pedro.molinanoa@gmail.com>
2024-06-19 20:14:59 -0500

    Fix ServerStatic MoDULE

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,7 +11,7 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 @Module({
   imports: [
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', '..', 'public'),
+      rootPath: join(__dirname, '..', 'public'),
     }),
     IncidentTypesModule,
     TypeUsersModule,
